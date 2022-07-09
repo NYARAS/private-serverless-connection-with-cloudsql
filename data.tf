be062b08e44e1712c3fbad83b1b9530032f174de
Calvine Otieno <nyarangaotieno@gmail.com>
2022-07-09 14:53:48 +0300

    chore: add required variables
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- data.tf
+++ data.tf
@@ -1,5 +1,3 @@
-# ======================== DATA SOURCE DEFINITION ================= #
-
 # It collects the different regions available within GCP.
 data "google_compute_regions" "available" {
 }
--- variables.tf
+++ variables.tf
@@ -225,3 +225,23 @@ variable "master_instance_name" {
   type        = string
   default     = ""
 }
+
+variable "sql_user_name" {
+  type        = string
+  description = "(Required) The name of the user."
+}
+
+variable "cloud_sql_instance_name" {
+  type        = string
+  description = "(Required) The name of the Cloud SQL instance the user will be created on."
+}
+
+# ==== Cloud SQL User ==== #
+variable "sql_user_password" {
+  type        = string
+  description = "(Required) The password for the user."
+	validation {
+		condition = var.sql_user_password != null && var.sql_user_password != ""
+		error_message = "The Cloud SQL user password must not be empty."
+	}
+}
