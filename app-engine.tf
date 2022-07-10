4da52a0fd5c230301060e08910a3d69381d824ea
Calvine Otieno <nyarangaotieno@gmail.com>
2022-07-10 21:46:44 +0300

    chore: add rediness and liveness check
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- app-engine.tf
+++ app-engine.tf
@@ -1,10 +1,22 @@
 resource "google_app_engine_flexible_app_version" "appengine_flexible_automatic_scaling" {
   runtime = var.runtime
   readiness_check {
-
+    path              = var.readiness_path
+    host              = var.readiness_host
+    failure_threshold = var.readiness_failure_threshold
+    success_threshold = var.readiness_success_threshold
+    check_interval    = var.readiness_check_interval
+    timeout           = var.readiness_timeout
+    app_start_timeout = var.readiness_app_start_timeout
   }
   liveness_check {
-
+    path              = var.liveness_path
+    host              = var.liveness_host
+    failure_threshold = var.liveness_failure_threshold
+    success_threshold = var.liveness_success_threshold
+    check_interval    = var.liveness_check_interval
+    timeout           = var.liveness_timeout
+    initial_delay     = var.liveness_initial_delay
   }
   service = var.service
 
